7a6d12fcb9e22ee49457a5bb58ef38b8bc546fb6
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-11-22 21:37:41 +0100

    update week-2.py assigment

--- week-2.py
+++ week-2.py
@@ -7,10 +7,15 @@ my_list.append(10)
 my_list.append(20)
 my_list.append(30)
 my_list.append(40)
+my_list.append(40)
+my_list.append(40)
+my_list.append(40)
+
 my_list.insert(0, 15)
 
 extended_list = [50, 60, 70]
 my_list.extend(extended_list)
 #my_list.pop()
 sorted(my_list)
+print(my_list.count(40))
 print(my_list)
