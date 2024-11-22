df42ce5db16b94cf48903d56a733c21c6d027990
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-11-22 20:28:31 +0100

    update week-2.py assigment

---
+++ week-2.py
@@ -0,0 +1,16 @@
+"""Python Week 2 Assignment"""
+
+my_list = []
+
+
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+my_list.insert(0, 15)
+
+extended_list = [50, 60, 70]
+my_list.extend(extended_list)
+#my_list.pop()
+sorted(my_list)
+print(my_list)
