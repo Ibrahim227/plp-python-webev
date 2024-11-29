c5cade26dc4423be39b7f399e8ff75350cde56ee
Ibrahim <mamansani.ibrahim01@gmail.com>
2024-11-29 16:52:31 +0100

    update week-4

--- week-4.py
+++ week-4.py
@@ -1,5 +1,5 @@
 try:
     with open("data.txt", "w", encoding="utf-8") as file:
-        file.write("Welcome to week 4!")
-except FileNotFoundError:
+        file.write("Welcome to week 4!")  # write "Welcome to week 4" to the file
+except FileNotFoundError:  # Handle exception in case the file is not found
     print("File Not Found!")
